6d0d2e173ab5bad9056ef06391c5246a8a1892fa
byunn90 <kayhanturk90@Live.com>
2024-05-03 19:41:18 +1000

    api call

--- my-currency-converter/src/App.js
+++ my-currency-converter/src/App.js
@@ -1,6 +1,29 @@
-// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useEffect, useState } from "react";
 
 export default function App() {
+  function CheckOut() {
+    const [exchangeRate, setExchangeRate] = useState(null);
+    const [error, setError] = useState(null);
+    useEffect(function () {
+      async function getEuro() {
+        try {
+          const response = await fetch(
+            `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+          );
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          console.log(data);
+        } catch (error) {
+          setError(error.message);
+          console.error("error with Api call", error);
+        }
+      }
+      getEuro();
+    }, []);
+  }
+
   return (
     <div>
       <input type="text" />
@@ -17,6 +40,7 @@ export default function App() {
         <option value="INR">INR</option>
       </select>
       <p>OUTPUT</p>
+      <CheckOut />;
     </div>
   );
 }
