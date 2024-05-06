3d40f809849fd0c73c6aecf720d5e2dd8cc4e847
byunn90 <kayhanturk90@Live.com>
2024-05-06 21:26:59 +1000

    testing onchanges

--- my-currency-converter/src/App.js
+++ my-currency-converter/src/App.js
@@ -1,35 +1,39 @@
 import { useEffect, useState } from "react";
-
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 export default function App() {
+  const [exchangeRate, setExchangeRate] = useState("EUR");
+  const [selectedExchangeRate, setSelectedExchangeRATE] = useState("USD");
+
   function CheckOut() {
-    const [exchangeRate, setExchangeRate] = useState("USD");
     useEffect(
       function () {
         async function getEuro() {
           const response = await fetch(
-            `https://api.frankfurter.app/latest?amount=1&from=${exchangeRate}&to=${setExchangeRate}`
+            `https://api.frankfurter.app/latest?amount=1&from=${exchangeRate}&to=${selectedExchangeRate}`
           );
 
           const data = await response.json();
           console.log(data);
         }
-        console.log(setExchangeRate);
         getEuro();
+        // console.log(exchangeRate);
+        // console.log(selectedExchangeRate);
       },
       [setExchangeRate]
     );
   }
+  function selectedCCurr() {}
 
   return (
     <div>
-      <input type="text" />
-      <select onClick={}>
+      <input type="text" placeholder="Type in Amount $" />
+      <select onChange={(e) => console.log(setExchangeRate(e.target.value))}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select onClick={}>
+      <select>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
