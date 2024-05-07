aab00ad6e0cc021a32131df601d82c9f47f10e12
byunn90 <kayhanturk90@Live.com>
2024-05-07 21:33:19 +1000

    testing

--- my-currency-converter/src/App.js
+++ my-currency-converter/src/App.js
@@ -1,35 +1,28 @@
 import { useEffect, useState } from "react";
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 export default function App() {
-  const [exchangeRate, setExchangeRate] = useState("");
-  const [selectedExchangeRate, setSelectedExchangeRATE] = useState("");
+  const [exchangeRate, setExchangeRate] = useState("USD");
+  const [selectedExchangeRate, setSelectedExchangeRATE] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
   function CheckOut() {
-    useEffect(
-      function () {
+    useEffect(() => {
+      async function getEuro() {
         try {
-          async function getEuro() {
-            const response = await fetch(
-              `https://api.frankfurter.app/latest?amount=1&from=${exchangeRate}&to=${selectedExchangeRate}`
-            );
+          const response = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${exchangeRate}&to=${selectedExchangeRate}`
+          );
 
-            if (!response.ok)
-              throw new Error(
-                "something went wrong with fetching your currency"
-              );
+          const data = await response.json();
+          console.log(data);
+          const conversionRate = data.rates[selectedExchangeRate];
+          setSelectedExchangeRATE(conversionRate);
+        } catch (err) {}
+      }
 
-            const data = await response.json();
-            console.log(data);
-          }
-          getEuro();
-        } catch (err) {
-          console.log(err.message);
-        }
-        // console.log(exchangeRate);
-        // console.log(selectedExchangeRate);
-      },
-      [setExchangeRate]
-    );
+      getEuro();
+    }, [exchangeRate, selectedExchangeRate, amount]);
   }
   function selectedCCurr() {
     console.log("hi", exchangeRate);
@@ -38,22 +31,30 @@ export default function App() {
 
   return (
     <div>
-      <input type="text" placeholder="Type in Amount $" />
-      <select onChange={(e) => console.log(setExchangeRate(e.target.value))}>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={exchangeRate}
+        onChange={(e) => setExchangeRate(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={selectedExchangeRate}
+        onChange={(e) => setSelectedExchangeRATE(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">IINR</option>
       </select>
-      <p>OUTPUT</p>
-      <CheckOut />
-      <button onClick={selectedCCurr}></button>
+      <p>${setSelectedExchangeRATE}</p>
     </div>
   );
 }
