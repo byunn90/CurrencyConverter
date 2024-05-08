ffc7e19a52310f0ea607be39e65a538596c8434c
byunn90 <kayhanturk90@Live.com>
2024-05-08 22:36:50 +1000

    updating

--- my-currency-converter/src/App.js
+++ my-currency-converter/src/App.js
@@ -1,60 +1,47 @@
 import { useEffect, useState } from "react";
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 export default function App() {
-  const [exchangeRate, setExchangeRate] = useState("USD");
-  const [selectedExchangeRate, setSelectedExchangeRATE] = useState("EUR");
   const [amount, setAmount] = useState(1);
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
 
-  function CheckOut() {
-    useEffect(() => {
-      async function getEuro() {
-        try {
-          const response = await fetch(
-            `https://api.frankfurter.app/latest?amount=${amount}&from=${exchangeRate}&to=${selectedExchangeRate}`
-          );
-
-          const data = await response.json();
-          console.log(data);
-          const conversionRate = data.rates[selectedExchangeRate];
-          setSelectedExchangeRATE(conversionRate);
-        } catch (err) {}
+  useEffect(
+    function () {
+      async function convert() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[toCur]);
       }
-
-      getEuro();
-    }, [exchangeRate, selectedExchangeRate, amount]);
-  }
-  function selectedCCurr() {
-    console.log("hi", exchangeRate);
-    console.log("hi", selectedExchangeRate);
-  }
+      convert();
+    },
+    [amount, fromCur, toCur]
+  );
 
   return (
     <div>
       <input
         type="text"
         value={amount}
-        onChange={(e) => setAmount(Number(e.target.value))}
+        onChange={(e) => setAmount(e.target.value)}
       />
-      <select
-        value={exchangeRate}
-        onChange={(e) => setExchangeRate(e.target.value)}
-      >
+      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select
-        value={selectedExchangeRate}
-        onChange={(e) => setSelectedExchangeRATE(e.target.value)}
-      >
+      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">IINR</option>
       </select>
-      <p>${setSelectedExchangeRATE}</p>
+      <p>
+        {converted} {toCur}
+      </p>
     </div>
   );
 }
